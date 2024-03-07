c2bd14ee9913c491ee170b286c83eb881a8bd4dd
Hasan Al Reza, PhD <72579193+hasanwraeth@users.noreply.github.com>
2024-03-07 17:13:47 -0500

    Add files via upload

--- Spat.R
+++ Spat.R
@@ -20,8 +20,12 @@ library(monocle3)
 library(CellChat)
 library(viridis)
 
-#10X Visium------------------
+#Data---------------
 #InstallData("stxBrain")
+#InstallData("ssHippo")
+
+#10X Visium------------------
+
 brain <- LoadData("stxBrain", type = "anterior1")
 
 plot1 <- VlnPlot(brain, features = "nCount_Spatial", pt.size = 0.1) + NoLegend()
@@ -53,3 +57,242 @@ p1 + p2
 
 SpatialDimPlot(brain, cells.highlight = CellsByIdentities(object = brain, idents = c(2, 1, 4, 3,
                                                                                      5, 8)), facet.highlight = TRUE, ncol = 3)
+SpatialDimPlot(brain, interactive = TRUE)
+
+SpatialFeaturePlot(brain, features = "Sox9", interactive = TRUE)
+
+LinkedDimPlot(brain)
+
+de_markers <- FindMarkers(brain, ident.1 = 5, ident.2 = 6)
+SpatialFeaturePlot(object = brain, features = rownames(de_markers)[1:4], alpha = c(0.1, 1), ncol = 2)
+
+brain <- FindSpatiallyVariableFeatures(brain, assay = "SCT", features = VariableFeatures(brain)[1:1000],
+                                       selection.method = "moransi")
+
+top.features <- head(SpatiallyVariableFeatures(brain, selection.method = "moransi"), 6)
+SpatialFeaturePlot(brain, features = top.features, ncol = 2, alpha = c(0.1, 1))
+
+cortex <- subset(brain, idents = c(1, 2, 3, 4, 6, 7))
+# now remove additional cells, use SpatialDimPlots to visualize what to remove
+# SpatialDimPlot(cortex,cells.highlight = WhichCells(cortex, expression = image_imagerow > 400
+# | image_imagecol < 150))
+cortex <- subset(cortex, anterior1_imagerow > 400 | anterior1_imagecol < 150, invert = TRUE)
+cortex <- subset(cortex, anterior1_imagerow > 275 & anterior1_imagecol > 370, invert = TRUE)
+cortex <- subset(cortex, anterior1_imagerow > 250 & anterior1_imagecol > 440, invert = TRUE)
+
+p1 <- SpatialDimPlot(cortex, crop = TRUE, label = TRUE)
+p2 <- SpatialDimPlot(cortex, crop = FALSE, label = TRUE, pt.size.factor = 1, label.size = 3)
+p1 + p2
+
+allen_reference <- readRDS("./allen_cortex.rds")
+allen_reference <- SCTransform(allen_reference, ncells = 3000, verbose = FALSE) %>%
+  RunPCA(verbose = FALSE) %>%
+  RunUMAP(dims = 1:30)
+
+# After subsetting, we renormalize cortex
+cortex <- SCTransform(cortex, assay = "Spatial", verbose = FALSE) %>%
+  RunPCA(verbose = FALSE)
+# the annotation is stored in the 'subclass' column of object metadata
+DimPlot(allen_reference, group.by = "subclass", label = TRUE)
+
+anchors <- FindTransferAnchors(reference = allen_reference, query = cortex, normalization.method = "SCT")
+predictions.assay <- TransferData(anchorset = anchors, refdata = allen_reference$subclass, prediction.assay = TRUE,
+                                  weight.reduction = cortex[["pca"]], dims = 1:30)
+cortex[["predictions"]] <- predictions.assay
+
+DefaultAssay(cortex) <- "predictions"
+SpatialFeaturePlot(cortex, features = c("Astro", "Oligo"), pt.size.factor = 1.6, ncol = 2, crop = TRUE)
+
+cortex <- FindSpatiallyVariableFeatures(cortex, assay = "predictions", selection.method = "moransi",
+                                        features = rownames(cortex), r.metric = 5, slot = "data")
+top.clusters <- head(SpatiallyVariableFeatures(cortex, selection.method = "moransi"), 4)
+SpatialPlot(object = cortex, features = top.clusters, ncol = 2)
+
+SpatialFeaturePlot(cortex, features = c("Astro","Oligo"), pt.size.factor = 1, ncol = 2, crop = FALSE, alpha = c(0.1, 1))
+
+brain2 <- LoadData("stxBrain", type = "posterior1")
+brain2 <- SCTransform(brain2, assay = "Spatial", verbose = FALSE)
+
+brain.merge <- merge(brain, brain2)
+
+DefaultAssay(brain.merge) <- "SCT"
+VariableFeatures(brain.merge) <- c(VariableFeatures(brain), VariableFeatures(brain2))
+brain.merge <- RunPCA(brain.merge, verbose = FALSE)
+brain.merge <- FindNeighbors(brain.merge, dims = 1:30)
+brain.merge <- FindClusters(brain.merge, verbose = FALSE)
+brain.merge <- RunUMAP(brain.merge, dims = 1:30)
+
+DimPlot(brain.merge, reduction = "umap", group.by = c("ident", "orig.ident"))
+
+SpatialDimPlot(brain.merge)
+
+SpatialFeaturePlot(brain.merge, features = c("Ttr", "Sox9"))
+
+
+#Slide-seq------------------
+slide.seq <- LoadData("ssHippo")
+
+plot1 <- VlnPlot(slide.seq, features = "nCount_Spatial", pt.size = 0, log = TRUE) + NoLegend()
+slide.seq$log_nCount_Spatial <- log(slide.seq$nCount_Spatial)
+plot2 <- SpatialFeaturePlot(slide.seq, features = "log_nCount_Spatial") + theme(legend.position = "right")
+wrap_plots(plot1, plot2)
+
+slide.seq <- SCTransform(slide.seq, assay = "Spatial", ncells = 3000, verbose = FALSE)
+slide.seq <- RunPCA(slide.seq)
+slide.seq <- RunUMAP(slide.seq, dims = 1:30)
+slide.seq <- FindNeighbors(slide.seq, dims = 1:30)
+slide.seq <- FindClusters(slide.seq, resolution = 0.3, verbose = FALSE)
+
+plot1 <- DimPlot(slide.seq, reduction = "umap", label = TRUE)
+plot2 <- SpatialDimPlot(slide.seq, stroke = 0)
+plot1 + plot2
+
+SpatialDimPlot(slide.seq, cells.highlight = CellsByIdentities(object = slide.seq, idents = c(7, 13)), facet.highlight = TRUE)
+
+ref <- readRDS("./mouse_hippocampus_reference.rds")
+ref <- UpdateSeuratObject(ref)
+
+anchors <- FindTransferAnchors(reference = ref, query = slide.seq, normalization.method = "SCT",
+                               npcs = 50)
+predictions.assay <- TransferData(anchorset = anchors, refdata = ref$celltype, prediction.assay = TRUE,
+                                  weight.reduction = slide.seq[["pca"]], dims = 1:50)
+slide.seq[["predictions"]] <- predictions.assay
+
+DefaultAssay(slide.seq) <- "predictions"
+SpatialFeaturePlot(slide.seq, features = c("Dentate Principal cells", "CA3 Principal cells", "Entorhinal cortex",
+                                           "Endothelial tip", "Ependymal", "Oligodendrocyte"), alpha = c(0.1, 1))
+slide.seq$predicted.id <- GetTransferPredictions(slide.seq)
+Idents(slide.seq) <- "predicted.id"
+SpatialDimPlot(slide.seq, cells.highlight = CellsByIdentities(object = slide.seq, idents = c("CA3 Principal cells",
+                                                                                             "Dentate Principal cells", "Endothelial tip")), facet.highlight = TRUE)
+DefaultAssay(slide.seq) <- "SCT"
+slide.seq <- FindSpatiallyVariableFeatures(slide.seq, assay = "SCT", slot = "scale.data", features = VariableFeatures(slide.seq)[1:1000],
+                                           selection.method = "moransi", x.cuts = 100, y.cuts = 100)
+
+SpatialFeaturePlot(slide.seq, features = head(SpatiallyVariableFeatures(slide.seq, selection.method = "moransi"),
+                                              6), ncol = 3, alpha = c(0.1, 1), max.cutoff = "q95")
+
+devtools::install_github("dmcable/spacexr", build_vignettes = FALSE)
+
+library(spacexr)
+
+# set up reference
+ref <- readRDS("../data/mouse_hippocampus_reference.rds")
+ref <- UpdateSeuratObject(ref)
+Idents(ref) <- "celltype"
+
+# extract information to pass to the RCTD Reference function
+counts <- ref[["RNA"]]$counts
+cluster <- as.factor(ref$celltype)
+names(cluster) <- colnames(ref)
+nUMI <- ref$nCount_RNA
+names(nUMI) <- colnames(ref)
+reference <- Reference(counts, cluster, nUMI)
+
+# set up query with the RCTD function SpatialRNA
+slide.seq <- SeuratData::LoadData("ssHippo")
+counts <- slide.seq[["Spatial"]]$counts
+coords <- GetTissueCoordinates(slide.seq)
+colnames(coords) <- c("x", "y")
+coords[is.na(colnames(coords))] <- NULL
+query <- SpatialRNA(coords, counts, colSums(counts))
+
+RCTD <- create.RCTD(query, reference, max_cores = 8)
+RCTD <- run.RCTD(RCTD, doublet_mode = "doublet")
+slide.seq <- AddMetaData(slide.seq, metadata = RCTD@results$results_df)
+
+p1 <- SpatialDimPlot(slide.seq, group.by = "first_type")
+p2 <- SpatialDimPlot(slide.seq, group.by = "second_type")
+p1 | p2
+
+
+#10X Xenium------------------
+path <- "./Xenium_V1_FF_Mouse_Brain_Coronal_Subset_CTX_HP_outs/"
+# Load the Xenium data
+xenium.obj <- LoadXenium(path, fov = "fov")
+# remove cells with 0 counts
+xenium.obj <- subset(xenium.obj, subset = nCount_Xenium > 0)
+
+VlnPlot(xenium.obj, features = c("nFeature_Xenium", "nCount_Xenium"), ncol = 2, pt.size = 0)
+
+ImageDimPlot(xenium.obj, fov = "fov", molecules = c("Gad1", "Sst", "Pvalb", "Gfap"), nmols = 20000)
+
+ImageFeaturePlot(xenium.obj, features = c("Cux2", "Rorb", "Bcl11b", "Foxp2"), max.cutoff = c(25,
+                                                                                             35, 12, 10), size = 0.75, cols = c("white", "red"))
+cropped.coords <- Crop(xenium.obj[["fov"]], x = c(1200, 2900), y = c(3750, 4550), coords = "plot")
+xenium.obj[["zoom"]] <- cropped.coords
+# visualize cropped area with cell segmentations & selected molecules
+DefaultBoundary(xenium.obj[["zoom"]]) <- "segmentation"
+ImageDimPlot(xenium.obj, fov = "zoom", axes = TRUE, border.color = "white", border.size = 0.1, cols = "polychrome",
+             coord.fixed = FALSE, molecules = c("Gad1", "Sst", "Npy2r", "Pvalb", "Nrn1"), nmols = 10000)
+
+xenium.obj <- SCTransform(xenium.obj, assay = "Xenium")
+xenium.obj <- RunPCA(xenium.obj, npcs = 30, features = rownames(xenium.obj))
+xenium.obj <- RunUMAP(xenium.obj, dims = 1:30)
+xenium.obj <- FindNeighbors(xenium.obj, reduction = "pca", dims = 1:30)
+xenium.obj <- FindClusters(xenium.obj, resolution = 0.3)
+
+DimPlot(xenium.obj)
+FeaturePlot(xenium.obj, features = c("Foxp2", "Gfap"))
+ImageDimPlot(xenium.obj, cols = "polychrome", size = 0.75)
+
+p1 <- ImageFeaturePlot(xenium.obj, features = "Slc17a7", axes = TRUE, max.cutoff = "q90")
+p1
+
+crop <- Crop(xenium.obj[["fov"]], x = c(600, 2100), y = c(900, 4700))
+xenium.obj[["crop"]] <- crop
+p2 <- ImageFeaturePlot(xenium.obj, fov = "crop", features = "Slc17a7", size = 1, axes = TRUE, max.cutoff = "q90")
+p2
+
+devtools::install_github("dmcable/spacexr", build_vignettes = FALSE)
+
+library(spacexr)
+
+query.counts <- GetAssayData(xenium.obj, assay = "Xenium", slot = "counts")[, Cells(xenium.obj[["crop"]])]
+coords <- GetTissueCoordinates(xenium.obj[["crop"]], which = "centroids")
+rownames(coords) <- coords$cell
+coords$cell <- NULL
+query <- SpatialRNA(coords, query.counts, colSums(query.counts))
+
+# allen.corted.ref can be downloaded here:
+# https://www.dropbox.com/s/cuowvm4vrf65pvq/allen_cortex.rds?dl=1
+allen.cortex.ref <- readRDS("/brahms/shared/vignette-data/allen_cortex.rds")
+allen.cortex.ref <- UpdateSeuratObject(allen.cortex.ref)
+
+Idents(allen.cortex.ref) <- "subclass"
+# remove CR cells because there aren't enough of them for annotation
+allen.cortex.ref <- subset(allen.cortex.ref, subset = subclass != "CR")
+counts <- GetAssayData(allen.cortex.ref, assay = "RNA", slot = "counts")
+cluster <- as.factor(allen.cortex.ref$subclass)
+names(cluster) <- colnames(allen.cortex.ref)
+nUMI <- allen.cortex.ref$nCount_RNA
+names(nUMI) <- colnames(allen.cortex.ref)
+nUMI <- colSums(counts)
+levels(cluster) <- gsub("/", "-", levels(cluster))
+reference <- Reference(counts, cluster, nUMI)
+
+# run RCTD with many cores
+RCTD <- create.RCTD(query, reference, max_cores = 8)
+RCTD <- run.RCTD(RCTD, doublet_mode = "doublet")
+
+annotations.df <- RCTD@results$results_df
+annotations <- annotations.df$first_type
+names(annotations) <- rownames(annotations.df)
+xenium.obj$predicted.celltype <- annotations
+keep.cells <- Cells(xenium.obj)[!is.na(xenium.obj$predicted.celltype)]
+xenium.obj <- subset(xenium.obj, cells = keep.cells)
+
+xenium.obj <- BuildNicheAssay(object = xenium.obj, fov = "crop", group.by = "predicted.celltype",
+                              niches.k = 5, neighbors.k = 30)
+
+celltype.plot <- ImageDimPlot(xenium.obj, group.by = "predicted.celltype", size = 1.5, cols = "polychrome",
+                              dark.background = F) + ggtitle("Cell type")
+niche.plot <- ImageDimPlot(xenium.obj, group.by = "niches", size = 1.5, dark.background = F) + ggtitle("Niches") +
+  scale_fill_manual(values = c("#442288", "#6CA2EA", "#B5D33D", "#FED23F", "#EB7D5B"))
+celltype.plot | niche.plot
+
+table(xenium.obj$predicted.celltype, xenium.obj$niches)
+
+
+#Save------------------
+saveRDS(slide.seq,'slideseq.rds')
